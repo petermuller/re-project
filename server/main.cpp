3c2cc858c2998bd2d27d9c221dc39aca69f65113
Peter Muller <stickmanthe@gmail.com>
2015-04-14 11:16:50 -0400

    High level pseudocode for server driver

--- server/main.cpp
+++ server/main.cpp
@@ -6,39 +6,10 @@ using namespace std;
 
 int main()
 {
-    string s,es,ds;
-    cout << "Server testing!" << endl << endl;
-    Server sv;
-    Coding c;
-    cout << "All Song Data Test" << endl;
-    s = sv.listAllSongData();
-    es = c.encode(s);
-    ds = c.decode(es);
-    cout << s << endl << endl << es << endl << endl << ds << endl;
-    cout << "Albums test" << endl;
-    s = sv.listAlbums();
-    es = c.encode(s);
-    ds = c.decode(es);
-    cout << s << endl << endl << es << endl << endl << ds << endl;
-    cout << "Artists test" << endl;
-    s = sv.listArtists();
-    es = c.encode(s);
-    ds = c.decode(es);
-    cout << s << endl << endl << es << endl << endl << ds << endl;
-    cout << "Lengths test" << endl;
-    s = sv.listLengths();
-    es = c.encode(s);
-    ds = c.decode(es);
-    cout << s << endl << endl << es << endl << endl << ds << endl;
-    cout << "Processes test" << endl;
-    s = sv.listProcesses();
-    es = c.encode(s);
-    ds = c.decode(es);
-    cout << s << endl << endl << es << endl << endl << ds << endl;
-    cout << "Songs test" << endl;
-    s = sv.listSongs();
-    es = c.encode(s);
-    ds = c.decode(es);
-    cout << s << endl << endl << es << endl << endl << ds << endl;
+    //Create socket connection
+    //Get input from client
+    //Parse info from client
+    //Give data to client after encoding it
+    //repeat
     return 0;
 }
