1e306221ff248ec68acac323c10e69557f46bd9e
Peter Muller <stickmanthe@gmail.com>
2015-04-16 08:32:20 -0400

    Starting winsock2 code

--- server/main.cpp
+++ server/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <winsock2.h>
 #include "Server.h"
 #include "Coding.h"
 
@@ -7,6 +8,14 @@ using namespace std;
 int main()
 {
     //Create socket connection
+    WSAData wsaData;
+    int code=WSAStartup(MAKEWORD(2,2),&wsaData);
+    if(code!=0){
+        cerr<<"WSAStartup Failed"<<endl;
+        return 1;
+    }
+
+
     //Get input from client
     //Parse info from client
     //Give data to client after encoding it
