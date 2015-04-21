97b190aca7b67aaf3c347b983d23341a438edb7d
Kirstie Failey <kirstie.failey@gmail.com>
2015-04-21 08:10:30 -0400

    Added encoding/decoding

--- client/main.cpp
+++ client/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+#include <sstream>
+#include <string>
 #include <conio.h>
 #include <windows.h>
+
 
 #include "Client.h"
 #include "Coding.h"
@@ -12,8 +15,10 @@ int main()
     cout << "opening socket!" << endl;
         //request listening socket from server (winsock client) add library libws2_32.a <-winsoc library
         //fill this out later!
-   int menuOption = 99; // option entered
-    while (menuOption != 0){
+    long menuOption = 99;       // option entered
+    string es, ds;              //variables for (e)ncode and (d)ecode strings
+    Coding c;                   // coding variable
+    while (menuOption > 0){
          //list the menu
         cout << "Menu: " <<  endl;
         cout << "0. Exit Program" << endl;
@@ -39,17 +44,40 @@ int main()
         cin >> menuOption;
         cout << endl;
 
+        cout << "option= " << menuOption << endl;
+
+        //convert number to string
+        std::ostringstream ss;  //string variable
+        ss << menuOption;       //convert long to string
+        string strMenuOption = ss.str(); //load it into variable to use throughout the project
+
+        //test the string variable
+        cout << "this is the menu option you chose: " << endl;
+        cout << strMenuOption << '\n';
+
         //encode option
+        cout << "Encoding...." << endl;
+        es = c.encode(strMenuOption);
+        cout << "Encoded string is: " << es << endl;
+        Sleep(120);
 
         //send to server (convert to string for ecoding method
+        cout << "Sending to server....Waiting" << endl;
+        Sleep(180);
 
         //recieve from server (listening the
+        cout << "Receiving!" << endl;
+        Sleep (120);
 
         //decode
+        cout << "Decoding..." << endl;
+        ds = c.decode(es);
+        Sleep (60);
 
-        //print
+        cout << "decoded string is " << ds << endl;
+        Sleep (3600);
     }
-    cout << "closing"
+    cout << "closing" << endl;
     cout << "Thank you for using ARMband Technologies!" << endl;
     cout << "Exiting...";
     Sleep(3600);
